08e1a798985c39f48d0c416732908d2b5f6c73f3
joeaoregan <joe.oregan@gmail.com>
2025-11-22 00:01:24 +0000

    wk11 ex9 regex

--- Exercises/Wk11 Ex9 Regular Expressions/q4_logs.py
+++ Exercises/Wk11 Ex9 Regular Expressions/q4_logs.py
@@ -1,3 +1,24 @@
 # A00258304
 
 import re
+
+filename = input("Enter the filename: ")
+
+status_codes_dict = {}
+
+regex = re.compile(r"(HTTP/1.[01])\" (\d{3})")
+
+with open(filename) as logfile:
+    for line in logfile:
+        match = regex.search(line)
+
+        if match:
+            # print(match.group(1), match.group(2))
+            status_codes_dict[match.group(2)] = status_codes_dict.get(match.group(2), 0) + 1
+            
+
+# print(status_codes_dict)
+print("Code Frequency")
+for code in sorted(status_codes_dict):
+    print(f"{str(code):6} {status_codes_dict[code]:>6}")
+
